349bbc1467246d68abc0eb6bc72ea0deea01c2fa
edaild <ch5183sb@gmail.com>
2026-05-09 13:58:29 +0900

    [Feat] 배틀씬 채력바 추가

--- Assets/Scripts/NewBattleSystem/Enemy/EnemySystem.cs
+++ Assets/Scripts/NewBattleSystem/Enemy/EnemySystem.cs
@@ -8,13 +8,16 @@ public class EnemySystem : MonoBehaviour
     public string Enmey_Name;
     public int Enemy_Levle;
     public int Enemy_MaxHelth;
-    public int Enemy_CurrentHelth;
     public int Enemy_Damage;
     public int Enemy_Speed;
     public GameObject EnemyPrefab;
 
-    public Button ThisEnemyButton;
-    public PlayerBattleSystem _playerBattleSystem;
+    [Header("Helth")]
+    public int Enemy_CurrentHelth;
+    public Slider enemyHelthSlider;
+    [SerializeField] private float smoothSpeed = 5f;
+
+
 
     private void Start()
     {
@@ -25,7 +28,18 @@ public class EnemySystem : MonoBehaviour
         Enemy_Speed = enemySO.EnemySpeed;
 
         Enemy_CurrentHelth = Enemy_MaxHelth;
+    }
 
-        _playerBattleSystem = Object.FindAnyObjectByType<PlayerBattleSystem>();
+    private void Update()
+    {
+        enemyHelthSlider.minValue = 0;
+        enemyHelthSlider.maxValue = Enemy_MaxHelth;
+
+        HelthUI();
+    }
+
+    private void HelthUI()
+    {
+        enemyHelthSlider.value = Mathf.Lerp(enemyHelthSlider.value, Enemy_CurrentHelth, smoothSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/NewBattleSystem/Player/PlayerSystem.cs
+++ Assets/Scripts/NewBattleSystem/Player/PlayerSystem.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerSystem : MonoBehaviour
 {
     public PlayerSO playerSO; 
     public string player_Name;
     public int player_MaxHelth;
-    public int player_CurrentHelth;
     public int player_Damage;
     public int player_Speed;
     public GameObject playerPrefab;
     public Animator playerAnimator;
+
+    [Header("Helth")]
+    public int player_CurrentHelth;
+    public Slider playerHelthSlider;
+    [SerializeField] private float smoothSpeed = 5f;
 
     private void Start()
     {
@@ -19,5 +25,18 @@ public class PlayerSystem : MonoBehaviour
         player_Speed = playerSO.playerSpeed;
 
         player_CurrentHelth = player_MaxHelth;
+    }
+
+    private void Update()
+    {
+        playerHelthSlider.minValue = 0;
+        playerHelthSlider.maxValue = player_MaxHelth;
+
+        HelthUI();
+    }
+
+    private void HelthUI()
+    {
+        playerHelthSlider.value = Mathf.Lerp(player_CurrentHelth, player_CurrentHelth, smoothSpeed * Time.deltaTime);
     }
 }
