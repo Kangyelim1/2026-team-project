07fb421c7234ec5bfa52bbd7e7c6f70907bee5a4
edaild <ch5183sb@gmail.com>
2026-05-21 16:36:43 +0900

    [Feat] 스킬, 아이템 데이터SO 자동화 튤 추가및 데이터 삽입

---
+++ Assets/Scripts/Skill/NewSkillData.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class NewSkillData
+{
+    public int SkillID;
+    public string SkillName;
+    public string SkillType;
+    public string Description;
+    public int Power;
+    public int BlockAmount;
+    public int LifeStealAmount;
+    public int CostAP;
+    public string TargetType;
+}
