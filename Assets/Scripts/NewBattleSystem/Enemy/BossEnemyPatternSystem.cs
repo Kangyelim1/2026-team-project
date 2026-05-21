6f569d4ca5ee6eb4f99a694e546380d95c37b43f
edaild <ch5183sb@gmail.com>
2026-05-21 23:38:13 +0900

    [Feat] 보스전 난무 이동 연출 추가

--- Assets/Scripts/NewBattleSystem/Enemy/BossEnemyPatternSystem.cs
+++ Assets/Scripts/NewBattleSystem/Enemy/BossEnemyPatternSystem.cs
@@ -1,6 +1,7 @@
 using JetBrains.Annotations;
 using System.Collections;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class BossEnemyPatternSystem : MonoBehaviour
 {
@@ -8,10 +9,12 @@ public class BossEnemyPatternSystem : MonoBehaviour
     public EnemyAttackSystem _enemyAttackSystem;
     public EnemySystem _enemySystem;
     public PlayerSystem _playerSystem;
+    public NewBattleManager _battleManager;
 
     private void Start()
     {
         _enemyBattleSystem = Object.FindAnyObjectByType<EnemyBattleSystem>();
+        _battleManager = Object.FindAnyObjectByType<NewBattleManager>();
     }
 
     private void Update()
@@ -41,11 +44,11 @@ public class BossEnemyPatternSystem : MonoBehaviour
         _enemySystem.Boss_Image.gameObject.SetActive(true);
         Debug.Log("순간 이동 완료");
         yield return new WaitForSeconds(0.1f);
-        int currentDamage = _enemySystem.Enemy_CurrentHelth + 50;
+        int currentDamage = _enemySystem.Enemy_Damage + 50;
         _playerSystem.HitEffect.gameObject.SetActive(true);
         TakeDamage(currentDamage);
        yield return new WaitForSeconds(1f);
-        _playerSystem.HitEffect.gameObject.SetActive(true);
+       if(_playerSystem != null) _playerSystem.HitEffect.gameObject.SetActive(false);
         _enemySystem.transform.position = _enemyAttackSystem.enemyStartPosition;
         yield return new WaitForSeconds(1f);
         _enemyBattleSystem._battleManager.EndEnemyTurn();
@@ -55,16 +58,41 @@ public class BossEnemyPatternSystem : MonoBehaviour
     public IEnumerator Wonnim03()   // 4번의 배기 공격
     {
         Debug.Log("난무 진행");
-        int currentDamage = _enemySystem.Enemy_CurrentHelth + 10;
+
+        while (Vector3.Distance(_enemySystem.transform.position, _enemyAttackSystem.TargetPlayer.transform.position) > 0.1f)
+        {
+            Vector3 direction = (_enemyAttackSystem.TargetPlayer.transform.position - _enemySystem.transform.position).normalized;
+            _enemySystem.transform.position += direction * _enemySystem.Enemy_Speed * Time.deltaTime;
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.2f);
+
+        int currentDamage = _enemySystem.Enemy_Damage + 10;
         for (int i = 0; i < 4; i++)
         {
             _playerSystem.HitEffect.gameObject.SetActive(true);
             TakeDamage(currentDamage);
-            yield return new WaitForSeconds(1f);
-            _playerSystem.HitEffect.gameObject.SetActive(false);
+            yield return new WaitForSeconds(0.1f);
+            if (_playerSystem != null) _playerSystem.HitEffect.gameObject.SetActive(false);
 
             yield return null;
         }
+
+        while (Vector3.Distance(_enemySystem.transform.position, _enemyAttackSystem.enemyStartPosition) > 0.1f)
+        {
+            Vector3 backDirection =
+                (_enemyAttackSystem.enemyStartPosition - _enemySystem.transform.position).normalized;
+
+            _enemySystem.transform.position +=
+                backDirection * _enemySystem.Enemy_Speed * Time.deltaTime;
+
+            yield return null;
+        }
+
+        _enemySystem.transform.position = _enemyAttackSystem.enemyStartPosition;
+
         yield return new WaitForSeconds(1f);
         _enemyBattleSystem._battleManager.EndEnemyTurn();
     }
@@ -80,9 +108,22 @@ public class BossEnemyPatternSystem : MonoBehaviour
     {
         if(_playerSystem.player_CurrentHelth > 0)
         {
+            Debug.Log(Damage);
             _playerSystem.player_CurrentHelth -= Damage;
             _playerSystem.player_CurrentHelth = Mathf.Clamp(_playerSystem.player_CurrentHelth, 0, Damage);
+            if (_playerSystem.player_CurrentHelth <= 0)
+            {
+                Debug.Log("플레이어 사망");
+                StartCoroutine(DiePlayer());
+            }
         }
+    }
+
+    IEnumerator DiePlayer()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Destroy(_playerSystem.gameObject);
+        _battleManager.spawnedPlayer = null;
     }
 }
 
