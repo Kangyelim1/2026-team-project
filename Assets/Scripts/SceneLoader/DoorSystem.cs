842451301c02d54c7bc69521cd877c3e8e5fbf05
Hodawn <happygy925@naver.com>
2026-06-11 02:31:57 +0900

    Feat : "미니게임 삭제 후 문 원상복구"

--- Assets/Scripts/SceneLoader/DoorSystem.cs
+++ Assets/Scripts/SceneLoader/DoorSystem.cs
@@ -1,53 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DoorSystem : MonoBehaviour
 {
-    [Header("주파수 미션")]
-    public RadioMission radioMission;
-
-    [Header("키패드 미션")]
-    public KeypadMission keypadMission;
-
-    private bool canInteract;
-    public GameObject interactionText;
-
-    private void Update()
-    {
-        if (canInteract && Input.GetKeyDown(KeyCode.F))
-        {
-            // 주파수 미션이 연결되어 있으면 실행
-            if (radioMission != null)
-            {
-                radioMission.StartMission();
-            }
-
-            // 키패드 미션이 연결되어 있으면 실행
-            if (keypadMission != null)
-            {
-                keypadMission.StartMission();
-            }
-        }
-    }
+    [Header("이동할 다음 씬 이름")]
+    public string nextSceneName;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            canInteract = true;
-
-            if (interactionText != null)
-                interactionText.SetActive(true);
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            canInteract = false;
-
-            if (interactionText != null)
-                interactionText.SetActive(false);
+            SceneManager.LoadScene(nextSceneName);
         }
     }
 }
